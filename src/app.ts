e1117015b0431138018a4d0065de4fa7991dafc4
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-08 22:14:47 +0500

    customer modelga schema yozildi

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,52 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig, dbConfig, jwtConfig } from './config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { JwtModule } from '@nestjs/jwt';
+
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig,jwtConfig],
+    }),
+    JwtModule.register({
+      secret: 'my secret',
+      global: true,
+      signOptions: {
+        expiresIn: 60 * 15
+      }
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        try {
+          return {
+            dialect: 'postgres',
+            host: config.get('database.host'),
+            port: config.get<number>('database.port'),
+            username: config.get('database.user'),
+            password: config.get('database.password'),
+            database: config.get('database.dbName'),
+            models: [],
+            synchronize: true,
+            // sync: { force: true },
+            logging: console.log,
+            autoLoadModels: true,
+          };
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  ],
+  // providers: [{
+  //   useClass: CheckAuthGuard,
+  //   provide:APP_GUARD
+  // }]
+
 })
 export class AppModule {}
+
