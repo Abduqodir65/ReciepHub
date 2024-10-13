fa3d6016e01577101b09d954268648661ff0ca40
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-13 21:40:53 +0500

    frontga qoshimchala kiritldi

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -14,3 +14,35 @@ signUpBtn.addEventListener("click", () => {
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+const fileInput = document.getElementById('fileInput');
+const imagePreview = document.getElementById('imagePreview');
+const imageElement = imagePreview.querySelector('.image-preview__image');
+const defaultText = imagePreview.querySelector('.image-preview__default-text');
+const fileLabel = document.querySelector('.custom-file-upload');
+
+fileInput.addEventListener('change', function() {
+    const file = this.files[0];
+    
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function() {
+            imageElement.setAttribute('src', this.result);
+            imageElement.style.display = 'block';
+            defaultText.style.display = 'none';
+
+            // Tugmani matnini o'zgartiramiz
+            fileLabel.textContent = 'Edit Picture';
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        imageElement.setAttribute('src', '');
+        imageElement.style.display = 'none';
+        defaultText.style.display = 'block';
+
+        // Tugmani matnini qaytarib qo'yamiz
+        fileLabel.textContent = 'Choose Profile Picture';
+    }
+});
