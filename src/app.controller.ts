4cd9b319298087e8c94dd3c8dfb1ecfe2e134522
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-13 21:59:57 +0500

    hamma filelar uchun render qilindi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,4 +20,27 @@ export class AppController {
         return { contactInfo: 'You can contact us at example@example.com.' };
     }
 
+    @Get('/elements')
+    @Render('elements')
+    element() {
+        return { info: 'this is the login page'}
+    }
+
+    @Get('/blog-post')
+    @Render('blog-post')
+    blog_post() {
+        return { info: 'this is the login page'}
+    }
+    @Get('/receipe-post')
+    @Render('reciepe-post')
+    resiep_post() {
+        return { info: 'this is the login page'}
+    }
+
+    @Get('/login')
+    @Render('login')
+    login() {
+        return { info: 'this is the login page'}
+    }
+
 }
